3ea610d86bdb874cc6a79dd7259fee9d6cad1be3
Neeraj M <neruz4life@gmail.com>
2025-05-29 19:50:08 +0530

    Screenshots added for all four HW questions

--- Homework 1/[1] Grade Calculator (U).py
+++ Homework 1/[1] Grade Calculator (U).py
@@ -4,9 +4,9 @@ if mark >= 90:
     grade = 'A'
 elif mark > 80 and mark < 90:
     grade = 'B'
-elif mark > 70 and mark <= 80:
+elif mark >= 70 and mark < 80:
     grade = 'C'
-elif mark > 60 and mark <= 70:
+elif mark >= 60 and mark < 70:
     grade = 'D'
 else:
     grade = 'E'
