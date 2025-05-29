57f3fb1a5ecf7d3deecc42217447a4f9c2c23678
Neeraj M <neruz4life@gmail.com>
2025-05-29 19:55:29 +0530

    fixed errors in 3rd answer

--- Homework 1/[3] Multiplication table.py
+++ Homework 1/[3] Multiplication table.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number: "))
 times = int(input("Enter the number of times to multiply: "))
-print("Multiplication table for", number,"from 1 to 10:")
+print("Multiplication table for", number,"from 1 to ",times,":")
 for i in range(1, times + 1):
     result = number * i
     print(number,"x", i, "=", result)
